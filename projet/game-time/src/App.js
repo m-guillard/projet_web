c99d1181d0bce1af66ee7b3cc3b080c310692d5c
Marion G. <ma86gui@gmail.com>
2025-03-23 11:23:41 +0100

    Ajout bibliothèque REACT pour pouvoir lancer et tester notre projet, visuel de Login implémenté

---
+++ projet/game-time/src/App.js
@@ -0,0 +1,12 @@
+import Login from './pages/Login.js'
+import './App.css';
+
+function App() {
+  return (
+        <div>
+          <Login /> {/* Affichage de la page Login */}
+        </div>
+      );
+}
+
+export default App;
---
+++ projet/game-time/src/pages/Login.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import "../styles/login.css";
+
+function Champ({nom, type_champ="text", mdp=false}){
+    return (
+    <ul>
+        <li><label>{nom}</label></li>
+        <li><input type={type_champ}/></li>
+        { mdp && <li className="oubli"><a href="https://google.com">Mot de passe oublié</a></li>}
+     </ul>)
+}
+
+function Titre({nom}){
+    return(<h3>{nom}</h3>)
+}
+
+function Bouton({nom}){
+    return(<input type="submit" name={nom} />)
+}
+
+function Login(){
+    return(<div id="fenetre">
+        <div className="formulaire">
+            <Titre nom={"Connexion"} />
+            <form action="" method="POST">
+                <Champ nom={"Nom d'utilisateur"} />
+                <Champ nom={"Mot de passe"} type_champ="password" />
+                <Bouton nom={"Se connecter"} />
+            </form>
+        </div>
+        <div className="separator"></div>
+        <div className="formulaire">
+            <Titre nom={"Inscription"} />
+            <form action="" method="POST">
+                <Champ nom={"Nom d'utilisateur"} />
+                <Champ nom={"Mail"} type_champ="email"/>
+                <Champ nom={"Date de naissance"} type_champ="date" />
+                <Champ nom={"Mot de passe"} type_champ="password" />
+                <Champ nom={"Confirmation du mot de passe"} type_champ="password" />
+                <Bouton nom={"S'inscrire"} />
+            </form>
+        </div>
+    </div>)
+}
+
+export default Login;
+
