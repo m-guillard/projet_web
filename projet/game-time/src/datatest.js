250af4b22e83c46683971df4bf5c501ec487dfd7
AD2305 <anais.dagnet5@gmail.com>
2025-03-26 16:09:35 +0100

    Search menu deroulant

---
+++ projet/game-time/src/datatest.js
@@ -0,0 +1,51 @@
+const Profil = [
+    {
+      id: 1,
+      imgPath: "/assets/img/1.png",
+      name: "Jane Doe",
+      email: "janedoe@gmail.com",
+      address: "New Delhi, India",
+    },
+    {
+      id: 2,
+      imgPath: "/assets/img/2.png",
+      name: "Mary Rosamund",
+      email: "agra@rosie.com",
+      address: "Tbilisi, India",
+    },
+    {
+      id: 3,
+      imgPath: "/assets/img/3.png",
+      name: "Sherlock Watson",
+      email: "irene@johnlock.com",
+      address: "Baker Street, India",
+    },
+    {
+      id: 4,
+      imgPath: "/assets/img/4.png",
+      name: "John Holmes",
+      email: "mary@johnlock.com",
+      address: "Baker Street, India",
+    },
+    {
+      id: 5,
+      imgPath: "/assets/img/5.png",
+      name: "Mycroft Lestrade",
+      email: "britishgovt@gmail.com",
+      address: "London, India",
+    },
+  ];
+  
+
+const Game= [
+      { id: 1, name: "The Legend of Zelda", genre: "Aventure", rating: 9.5 },
+      { id: 2, name: "God of War", genre: "Action", rating: 9.8 },
+      { id: 3, name: "Minecraft", genre: "Sandbox", rating: 9.0 },
+      { id: 4, name: "Cyberpunk 2077", genre: "RPG", rating: 7.5 },
+    ];
+
+
+const initialDetails= {Game,Profil};
+
+
+export default initialDetails;
--- projet/game-time/src/pages/search.js
+++ projet/game-time/src/pages/search.js
@@ -3,27 +3,94 @@ import "../styles/search.css";
 import { useState } from 'react';
 
 
-function Bar(){
+function Cardgame({game}){
+  return(
+    <div class="boxgame">
+      <div>
+        <h2>{game.name}</h2>
+        <p>{game.genre}</p>
+      </div>
+    </div>
+  );
+}
+function Cardprofil({profil}){
+  return(
+  <div class="boxgame">
+      <div>
+        <h2>{profil.name}</h2>
+        <p>{profil.email}</p>
+      </div>
+    </div>);
+}
+
+function Searchlist({ person = [], games = [] }){
+  const filtgame=games.map(game =>  <Cardgame key={game.id} game={game} />);
+  const filtperson=person.map(profil =>  <Cardprofil key={profil.id} profil={profil} />);
+  return(
+    <div>
+      <h3>🎮 Games</h3>
+      {filtgame.length > 0 ? filtgame : <p>Aucun jeu trouvé.</p>}
+
+      <h3>👤 Profils</h3>
+      {filtperson.length > 0 ? filtperson : <p>Aucun profil trouvé.</p>}
+    </div>
+  )
+}
+
+const Scroll = (props) => {
+  return( 
+    <div class="scroll-menu">
+      {props.children}
+    </div>	
+  );
+}
+
+export default function Search({datagame,dataprofil}) {
     const [value, setValue] = useState("");
-    return(<input type="text" placeholder="Search.."/>)
-}
+    const [searchShow, setSearchShow] = useState(false);
+    ///const [suggestions, setSuggestions] = useState([]);
+    const filteredgame = datagame.filter((item) =>
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
+    const filteredprofil = dataprofil.filter((item) =>
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
 
-function ThemeBox ({nom}){
-    return (<button>{nom}</button>);
-}
+    function search(){
+      if (searchShow){
+        return(<Scroll>
+          <Searchlist person={filteredprofil} games={filteredgame}/>
+        </Scroll>);
+      }
+      return null;
+    }
 
-export default function Search() {
-    return (<div id="search">
-                <div class="navbar">
-                    <span>Home</span>
-                    <span>Recherche :</span>
-                </div>
-
-                <div class="searchbar">
-                    <h1>Recherche</h1>
-                    <input type="text" placeholder="Search.."/>
-                </div>
-
+    return (
+    <div id="search">
+        <div class="navbar">
+            <span>Home</span>
+            <span>Recherche :</span>
         </div>
+
+        <div class="searchbar">
+            <h1 >Recherche</h1>
+            <input type="text" 
+              placeholder="Search.."
+              value={value}
+              onChange={(e) => {
+                setValue(e.target.value);
+                if(e.target.value===""){
+                  setSearchShow(false);
+                }
+                else {
+                  setSearchShow(true);
+                }
+              }}/>
+            <div>
+              {search()}
+            </div>
+        </div>
+        
+    </div>
     )
 }
