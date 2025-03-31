e778a8de746882ce188140f6d69911392c727989
AD2305 <anais.dagnet5@gmail.com>
2025-03-31 16:56:02 +0200

    filtre v2

--- projet/game-time/frontend/src/pages/search.js
+++ projet/game-time/frontend/src/pages/search.js
@@ -13,6 +13,7 @@ function Cardgame({game}){
     </div>
   );
 }
+
 function Cardprofil({profil}){
   return(
   <div class="boxgame">
@@ -23,20 +24,25 @@ function Cardprofil({profil}){
     </div>);
 }
 
-function Searchlist({ person = [], games = [] }){
+function Searchgame({ games = [] }){
   const filtgame=games.map(game =>  <Cardgame key={game.id} game={game} />);
-  const filtperson=person.map(profil =>  <Cardprofil key={profil.id} profil={profil} />);
   return(
     <div>
       <h3>🎮 Games</h3>
       {filtgame.length > 0 ? filtgame : <p>Aucun jeu trouvé.</p>}
+    </div>
+  )
+}
 
+function Searchprofil({ person = [] }){
+  const filtperson=person.map(profil =>  <Cardprofil key={profil.id} profil={profil} />);
+  return(
+    <div>
       <h3>👤 Profils</h3>
       {filtperson.length > 0 ? filtperson : <p>Aucun profil trouvé.</p>}
     </div>
   )
 }
-
 
 const Scroll = (props) => {
   return( 
@@ -51,12 +57,10 @@ export default function Search({datagame,dataprofil}) {
     const [searchShow, setSearchShow] = useState(false);
     const themes =["Horror","Action","Adventure","RPG","Sandbox","FPS","MOBA"];
     const [selectedOptions, setSelectedOptions] = useState([]);
+    const [selectedType, setselectedType] = useState([]);
 
-    const [filteredgame,setFilteredGames] =useState( datagame.filter((item) =>
-      item.name.toLowerCase().includes(value.toLowerCase()) ))
-    const filteredprofil = dataprofil.filter((item) =>
-      item.name.toLowerCase().includes(value.toLowerCase())
-    );
+    const [filteredgame,setFilteredGames] =useState( datagame.filter((item) =>item.name.toLowerCase().includes(value.toLowerCase()) ))
+    const filteredprofil = dataprofil.filter((item) =>item.name.toLowerCase().includes(value.toLowerCase()));
     
     useEffect(() => {
       let filtered = datagame.filter(item => item.name.toLowerCase().includes(value.toLowerCase()));
@@ -85,9 +89,16 @@ export default function Search({datagame,dataprofil}) {
       }
     }
 
-    function handleCheckboxChange(e) {
+    function handleCheckboxChangeGenre(e) {
       const { value, checked } = e.target;
       setSelectedOptions(prev =>
+        checked ? [...prev, value] : prev.filter(option => option !== value)
+      );
+    }
+
+    function handleCheckboxChangeType(e) {
+      const { value, checked } = e.target;
+      setselectedType(prev =>
         checked ? [...prev, value] : prev.filter(option => option !== value)
       );
     }
@@ -105,28 +116,62 @@ export default function Search({datagame,dataprofil}) {
       }
     }
 
+    function gameprofil(){
+      if (selectedType.includes("profil") && !selectedType.includes("game")){
+          return(<div class="game-container">
+            <Searchprofil  person={filteredprofil} />
+            </div>
+          );
+      }else if(selectedType.includes("game") && !selectedType.includes("profil")){
+        return(<div class="game-container">
+          <Searchgame  games={filteredgame} />
+        </div>);
+      }else{
+        return(<div class="game-container">
+          <Searchgame  games={filteredgame} />
+          <Searchprofil  person={filteredprofil} />
+          </div>
+        );
+      }       
+    }
+
     function result(){
       if (searchShow){
         return(
         <div class="result-container">
           <div class="filter-container">
             <h1>Filter</h1>
+            <input 
+              type="checkbox"
+              name="profil"
+              value="profil"
+              checked={selectedType.includes("profil")}
+              onChange={handleCheckboxChangeType}
+              defaultChecked={true}/>
+            <label>Profil</label>
+            <input 
+              type="checkbox"
+              name="game"
+              value="game"
+              checked={selectedType.includes("game")}
+              onChange={handleCheckboxChangeType}
+              defaultChecked={true}/>
+            <label>Game</label>
             <h2>Genres</h2>
             {themes.map((theme, index) => (
               <div key={theme}>
             <input 
-            type="checkbox"
-            name={theme}
-            value={theme}
-            checked={selectedOptions.includes(theme)}
-            onChange={handleCheckboxChange}/>
+              type="checkbox"
+              name={theme}
+              value={theme}
+              checked={selectedOptions.includes(theme)}
+              onChange={handleCheckboxChangeGenre}
+              />
             <label>{theme}</label>
             </div>))}
             
           </div>
-          <div class="game-container">
-            <Searchlist person={dataprofil} games={filteredgame} />
-          </div>
+          {gameprofil()}          
         </div>)
       }
     }
@@ -151,7 +196,6 @@ export default function Search({datagame,dataprofil}) {
               </button>
             
         </div>
-
           {boxtheme()}
           {result()}
     </div>
