b4514ef3c43ee6c6085fdfc97de0cbfecef1697d
Marion G. <ma86gui@gmail.com>
2025-04-02 11:44:50 +0200

    Enregistrement d'un utilisateur

--- projet/game-time/backend/routes/login.js
+++ projet/game-time/backend/routes/login.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const bcrypt = require("bcrypt");
 const router = express.Router();
 const users_db = require('../database/users')
 
 router.use(express.json());
+
+const createUser = async(username, mail, bday, pswd) => {
+    try {
+        const user = new users_db({
+            username: username,
+            password: pswd,
+            mail: mail,
+            birthday: bday
+        });
+
+        await user.save();
+    } catch (error) {
+        console.error('Erreur lors de la création du user :', error);
+    }
+};
+
 
 router.post('/', async (req, res) => {
     const data = req.body;
@@ -12,44 +29,33 @@ router.post('/', async (req, res) => {
 
         // On vérifier que userName n'est pas déjà dans la base
         var utilisateurs = await users_db.find({username: data["username"]});
-        console.log(utilisateurs);
+        // Regex pour verif entrée utilisateur
+        const mailRegex = new RegExp("^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$");
+        // Vérification dates
+        const date = new Date(data["birthday"]);
+        const date_jour = Date.now()
+
         if (utilisateurs.length){
             console.log("Il y a déjà un utilisateeeeeeur");
         }
-        else{
-            console.log("Bienvenue", data["usernameInsc"]);
-        }
-        // On vérifie que le mail est valide
-        const mailRegex = new RegExp("^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$")
-        if(!mailRegex.test(data["mail"])){
+        else if(!mailRegex.test(data["mail"])){
             console.log("Erreur adresse mail");
-        }else{
-            console.log("Mail good");
-        }
-        // On vérife que la date de naissance est valide
-        const dateRegex = new RegExp("^\d{4}-\d{2}-\d{2}$");
-        if (!dateRegex.test(data["birthday"])){
-            console.log("Problème format date");
-        }else{
-            const date = new Date(data["birthday"]);
-            const date_jour = Date.now()
-            
-            if(isNaN(date)){
-                console.log("Cette date n'existe pas");
-            }
-            else if(date>date_jour){
-                console.log("Tu n'es pas né");
-            }else if(date.getFullYear()<1900){
-                console.log("Tu es mort");
-            }else{
-                console.log("Date valide")
-            }
-            
-        }
-        // On vérifie que le mot de passe est le même que la confirmation de mot de passe
-        if(data["passwordpasswordInsc"]!=data["verifPassword"]){
+        }else if(isNaN(date)){
+          console.log("Cette date n'existe pas");
+        }else if(date>date_jour){
+            console.log("Tu n'es pas né.e");
+        }else if(date.getFullYear()<1900){
+            console.log("Tu es mort.e");
+        }else if(data["passwordInsc"]!==data["verifPassword"]){
             console.log("Mot de passe différent de la confirmation");
+        }else{
+            // On hash le mot de passe
+            pswd_hash = await bcrypt.hash(data["passwordInsc"], 10);
+            // Tout est validé, on enregistre dans la base de données
+            createUser(data["usernameInsc"], data["mail"], data["birthday"], pswd_hash);
         }
+
+
     }
     else if(data["page"] == "connexion"){
         // On vérifie qu'il existe le couple username/password
