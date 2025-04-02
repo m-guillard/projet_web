f7acd27f5fb7c953eef9d5fef265e2915dd31e38
AD2305 <anais.dagnet5@gmail.com>
2025-04-02 16:03:35 +0200

    connexion search back

--- projet/game-time/backend/database/games.js
+++ projet/game-time/backend/database/games.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     name: String,
     genre : String,
-    rating : Int16Array
+
 });
 
 const Game = mongoose.model("Game", userSchema);
